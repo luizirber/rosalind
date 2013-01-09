37564cf429b18395207a2506b66d57137242dee9
Luiz Irber <luiz.irber@gmail.com>
2013-01-09 18:07:13 -0200

    Solution to LCS problem

---
+++ lcs/lcs.py
@@ -0,0 +1,21 @@
+import os
+
+PROJPATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+
+SAMPLES = open(os.path.join(PROJPATH, 'data', 'rosalind_lcs.txt')).read().strip('\n').split()
+
+def lcs(s1, s2):
+    L = {}
+    z = 0
+
+    for i, c1 in enumerate(s1):
+        for j, c2 in enumerate(s2):
+            if c1 == c2:
+                L[(i, j)] = L.get((i-1, j-1), 0) + 1
+
+                if L[(i, j)] > z:
+                    z = L[(i, j)]
+                    ret = s1[i-z+1:i+1]
+    return ret
+
+print reduce(lcs, SAMPLES)
