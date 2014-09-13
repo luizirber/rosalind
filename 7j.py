2ce4a59698ab01a593b407106a99dfd7eb2369ba
Luiz Irber <luiz.irber@gmail.com>
2014-09-13 12:25:45 -0400

    Cleanup and some comments

--- 7j.py
+++ 7j.py
@@ -17,26 +17,34 @@ def naive_inverse_bwt(last_column, alphabet="$ACGT"):
 
 
 def inverse_bwt(last, alphabet="$ACGT"):
-    original = []
+    # calculate the first column
+    # it's just the last column (bwt), but sorted in lexicographical order
     first = sorted(last, key=lambda x: [alphabet.find(c) for c in x])
 
+    # small optimization: save the indexes for each character.
+    # for example, idx['a'] == [3,4,5,8] are all positions
+    # of char 'a' in the string.
+    # To get the third occurrence of 'a' just need to access
+    # idx['a'][2]
     first_idx = defaultdict(list)
     last_idx = defaultdict(list)
     for i, (cf, cl) in enumerate(zip(first, last)):
         first_idx[cf].append(i)
         last_idx[cl].append(i)
 
-    original.append(first[0])
-    original.append(last[0])
+    original = []
+    original.append(first[0])  # first[0] == '$'
 
-    next_row = first_idx[last[0]][0]
-    original.append(last[next_row])
+    next_row = 0
+    current_chr = last[next_row]
+    original.append(current_chr)
 
-    for i in range(len(last) - 3):
-        next_idx = next(i for (i, idx) in enumerate(last_idx[last[next_row]])
-                        if idx == next_row)
-        next_row = first_idx[last[next_row]][next_idx]
-        original.append(last[next_row])
+    for i in range(len(last) - 2):
+        next_idx = next(i for (i, idx) in enumerate(last_idx[current_chr])
+                          if idx == next_row)
+        next_row = first_idx[current_chr][next_idx]
+        current_chr = last[next_row]
+        original.append(current_chr)
 
     return "".join(reversed(original))
 
